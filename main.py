4de1c3a3d9b392de69e8d62784c30b90b869a538
Andrey Labanca <moraislabanca@gmail.com>
2023-04-13 01:43:26 -0300

    pronto para automação com makefile (make download, run, all)

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import glob
 import duckdb
 import os
 
-DB_NAME = 'database/dadosmg.duckdb'
-CSV_PATH = 'datasets/'
 
-# obtem lista de paths para arquivos CSV localizados no caminho CSV_PATH
-file_paths = [i.replace('\\', '/') for i in list(glob.iglob(f'{CSV_PATH}*.csv'))]
+DB_NAME = 'database/dadosmg.duckdb'
+DATASETS_DIR = 'despesa/'
+DATASET_DIR = 'datasets/'
+DATA_DIR = 'data/'
+DATA_PATH = DATASET_DIR + DATASETS_DIR + DATA_DIR
+
+# obtem lista de paths para arquivos CSV localizados no caminho DATA_PATH
+file_paths = [i.replace('\\', '/') for i in list(glob.iglob(f'{DATA_PATH}*.csv*'))]
 
 # paths de bases csv que sao separadas por anos
 file_paths_desp = [x for x in file_paths if "dm_empenho_desp_" in x]
@@ -33,8 +37,9 @@ def drop_tables(con=None):
 def tables_from_csv(file_paths):
 
     for file in file_paths:
-        _, tail = os.path.split(file)
-        name, file_extension = os.path.splitext(tail)
+        head, tail = os.path.split(file)
+        name = tail.split('.')[0]
+        _, file_extension = os.path.splitext(tail)
         con.execute(f"""CREATE TABLE '{name}' AS SELECT * FROM read_csv_auto('{file}')""")
         print(f"Arquivo {file} carregado para tabela {name}\n")
 
@@ -44,6 +49,8 @@ def append_from_csv(file_paths_append, tbl_agg_name):
     #tbl_agg_name = 'ft_despesa'
     df_agg = pd.DataFrame()
     num_linhas = 0
+    exec_error = False
+    files_error = []
 
     #Toma o primeiro arquivo da lista de csvs como definidor dos nomes das colunas e tipos
     temp_csv = con.sql(f"""SELECT * FROM '{file_paths_append[0]}' LIMIT 10 """)
@@ -61,19 +68,31 @@ def append_from_csv(file_paths_append, tbl_agg_name):
     for file in file_paths_append:
         print(f'Lendo:', file)
         df1 = con.execute(f"""SELECT * FROM '{file}' """).df()
-        con.execute(f"""INSERT INTO '{tbl_agg_name}' SELECT * FROM df1""")
-        print(f'Arquivo {file} concatenado na tabela {tbl_agg_name}\n')
+        try:
+            con.execute(f"""INSERT INTO '{tbl_agg_name}' SELECT * FROM df1""")
+            print(f'Arquivo {file} concatenado na tabela {tbl_agg_name}\n')
+
+        except:
+            print(f"ERRO: Arquivo {file} está vazio ou contém schema divergente dos demais.\n")
+            exec_error = True
+            files_error.append(file)
+
+
         num_linhas += len(df1)
 
     print('-------------------------------------------------------')
 
     print('Total de linhas tabelas lidas:', num_linhas)
-    print('Num linhas da tabela agregada:')
-    print(con.sql(f"""SELECT COUNT(*) FROM {tbl_agg_name}"""))
+    #print('Num linhas da tabela agregada:')
+    #print(con.sql(f"""SELECT COUNT(*) FROM {tbl_agg_name}"""))
 
+    #con.table(tbl_agg_name).show()
 
-    con.table(tbl_agg_name).show()
-
+    # alerta para falha no carregamento de arquivos. Arquivos somente com cabeçalhos geram esse erro.
+    if exec_error:
+        print(f"ATENÇÃO! os seguintes arquivos não foram carregados para a base de dados:")
+        print(files_error, sep='\n')
+        print('\n\n')
 
 
 def descbribe_db(con=None):
@@ -98,9 +117,10 @@ if __name__ == '__main__':
         drop_tables(con)
 
     tables_from_csv(file_paths)
+
     append_from_csv(file_paths_desp, 'dm_empenho_desp')
     append_from_csv(file_paths_ft, 'ft_despesa')
-    descbribe_db(con)
-    describe_table(con,'ft_despesa')
-    group_by_field(con, 'ft_despesa', 'ano_particao', 'vr_liquidado')
+    #descbribe_db(con)
+    #describe_table(con,'ft_despesa')
+    #group_by_field(con, 'ft_despesa', 'ano_particao', 'vr_liquidado')
 
